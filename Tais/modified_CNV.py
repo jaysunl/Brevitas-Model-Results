4c5d025e86d5cd820831fcd8443edf935d09a6e5
Tai Nguyen <65136598+TaiNguyenUCSD@users.noreply.github.com>
2021-09-03 09:57:34 -0700

    Delete Tais directory

--- Tais/modified_CNV.py
+++
@@ -1,7 +0,0 @@
-#Dividing numbers by 2 - Tai Nguyen
-CNV_OUT_CH_POOL = [(32, False), (32, True), (64, False), (64, True), (128, False), (128, False)]
-INTERMEDIATE_FC_FEATURES = [(128, 256), (256, 256)]
-LAST_FC_IN_FEATURES = 256
-LAST_FC_PER_OUT_CH_SCALING = False
-POOL_SIZE = 2
-KERNEL_SIZE = 3
